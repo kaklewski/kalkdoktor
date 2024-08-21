f6b9d9e6f7a9dec503bd55a9ca5a5954ba5c77bf
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-08-21 22:09:22 +0200

    feat: add system theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme, ThemeConfig } from '@chakra-ui/react'
 import './main.scss'
+
+const config: ThemeConfig = {
+	initialColorMode: 'system',
+	useSystemColorMode: true,
+}
+
+const theme = extendTheme({ config })
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<App />
 		</ChakraProvider>
 	</StrictMode>
