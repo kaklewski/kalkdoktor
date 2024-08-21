bfb81ce08d14d3c28b1b4a5da1f8a76620193695
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-08-21 21:34:03 +0200

    feat: move page layout to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
 import {
 	Box,
-	Card,
-	CardBody,
-	CardHeader,
 	Checkbox,
-	Divider,
-	Flex,
-	Heading,
-	Icon,
-	IconButton,
 	Radio,
 	RadioGroup,
-	Spacer,
 	Stack,
 	StackDivider,
-	Tab,
-	TabList,
-	TabPanel,
-	TabPanels,
-	Tabs,
 	Text,
 } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 
 import Navbar from './components/Navbar'
-import DetailsCard from './components/DetailsCard'
-import ResultCard from './components/ResultCard'
-import FavButton from './components/FavButton'
-import FormCard from './components/FormCard'
+
+import PageLayout from './layouts/PageLayout'
 
 export default function App() {
 	const lowRisk: string = 'Niskie ryzyko powikłań. Nie zaleca się leczenia.'
@@ -72,103 +56,87 @@ export default function App() {
 		setResultDescription(resultDescriptionText)
 	}
 
+	const title = (
+		<span>
+			Skala CHA<sub>2</sub>DS<sub>2</sub>-VASc
+		</span>
+	)
+
+	const formContent = (
+		<Stack divider={<StackDivider />} spacing={4}>
+			<Box>
+				<Checkbox value='1' colorScheme='teal' w='100%'>
+					Zastoinowa niewydolność serca / dysfunkcja lewej komory
+				</Checkbox>
+			</Box>
+			<Box>
+				<Checkbox value='1' colorScheme='teal' w='100%'>
+					Nadciśnienie tętnicze
+				</Checkbox>
+			</Box>
+			<Box>
+				<Checkbox value='1' colorScheme='teal' w='100%'>
+					Cukrzyca
+				</Checkbox>
+			</Box>
+			<Box>
+				<Checkbox value='1' colorScheme='teal' w='100%'>
+					Choroba naczyniowa (przebyty zawał serca, miażdżycowa
+					choroba tętnic obwodowych, blaszki miażdżycowe w aorcie)
+				</Checkbox>
+			</Box>
+			<Box>
+				<Checkbox value='2' colorScheme='teal' w='100%'>
+					Przebyty udar mózgu / TIA / incydent zakrzepowo-zatorowy
+				</Checkbox>
+			</Box>
+			<Box>
+				<RadioGroup defaultValue='0' colorScheme='teal'>
+					<Stack>
+						<Text>Płeć:</Text>
+						<Radio value='0' id='man'>
+							Mężczyzna
+						</Radio>
+						<Radio value='1'>Kobieta</Radio>
+					</Stack>
+				</RadioGroup>
+			</Box>
+			<Box>
+				<RadioGroup defaultValue='0' colorScheme='teal'>
+					<Stack>
+						<Text>Wiek:</Text>
+						<Radio value='0'>Mniej niż 65 lat</Radio>
+						<Radio value='1'>65 – 74 lata</Radio>
+						<Radio value='2'>75 lat lub więcej</Radio>
+					</Stack>
+				</RadioGroup>
+			</Box>
+		</Stack>
+	)
+
+	const description =
+		'Skala służąca do oceny ryzyka wystąpienia powikłań zakrzepowo–zatorowych u pacjentów z migotaniem przedsionków. Skala umożliwia wskazanie pacjentów z migotaniem przedsionków, u których konieczne jest wdrożenie terapii przeciwpłytkowej lub przeciwzakrzepowej.'
+
+	const source =
+		'Lorem ipsum dolor sit amet consecteturs adipisicing elit. Nihil, voluptate.'
+
+	const interpretation =
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus reiciendis aperiam placeat nobis, dolorum laborum, nemo, eos quidem esse ducimus expedita amet repellendus nesciunt enim. Quae pariatur numquam aliquam doloremque.'
+
 	return (
 		<>
 			<Navbar />
-			<Box
-				maxW='650px'
-				mx='auto'
-				p={4}
-				onChange={() => calculateResult(setResult)}>
-				<Stack spacing={8}>
-					<Flex>
-						<Heading as='h1'>
-							Skala CHA<sub>2</sub>DS<sub>2</sub>-VASc
-						</Heading>
-
-						<Spacer />
-
-						<FavButton />
-					</Flex>
-
-					<FormCard>
-						<Box>
-							<Checkbox value='1' colorScheme='teal' w='100%'>
-								Zastoinowa niewydolność serca / dysfunkcja lewej
-								komory
-							</Checkbox>
-						</Box>
-						<Box>
-							<Checkbox value='1' colorScheme='teal' w='100%'>
-								Nadciśnienie tętnicze
-							</Checkbox>
-						</Box>
-						<Box>
-							<Checkbox value='1' colorScheme='teal' w='100%'>
-								Cukrzyca
-							</Checkbox>
-						</Box>
-						<Box>
-							<Checkbox value='1' colorScheme='teal' w='100%'>
-								Choroba naczyniowa (przebyty zawał serca,
-								miażdżycowa choroba tętnic obwodowych, blaszki
-								miażdżycowe w aorcie)
-							</Checkbox>
-						</Box>
-						<Box>
-							<Checkbox value='2' colorScheme='teal' w='100%'>
-								Przebyty udar mózgu / TIA / incydent
-								zakrzepowo-zatorowy
-							</Checkbox>
-						</Box>
-						<Box>
-							<RadioGroup defaultValue='0' colorScheme='teal'>
-								<Stack>
-									<Text>Płeć:</Text>
-									<Radio value='0' id='man'>
-										Mężczyzna
-									</Radio>
-									<Radio value='1'>Kobieta</Radio>
-								</Stack>
-							</RadioGroup>
-						</Box>
-						<Box>
-							<RadioGroup defaultValue='0' colorScheme='teal'>
-								<Stack>
-									<Text>Wiek:</Text>
-									<Radio value='0'>Mniej niż 65 lat</Radio>
-									<Radio value='1'>65 – 74 lata</Radio>
-									<Radio value='2'>75 lat lub więcej</Radio>
-								</Stack>
-							</RadioGroup>
-						</Box>
-					</FormCard>
-
-					<ResultCard
-						result={result}
-						resultDescription={resultDescription}
-					/>
-
-					<DetailsCard
-						description='Skala służąca do oceny ryzyka
-										wystąpienia powikłań
-										zakrzepowo–zatorowych u pacjentów z
-										migotaniem przedsionków. Skala umożliwia
-										wskazanie pacjentów z migotaniem
-										przedsionków, u których konieczne jest
-										wdrożenie terapii przeciwpłytkowej lub
-										przeciwzakrzepowej.'
-						source='Lorem ipsum dolor sit amet consecteturs
-										adipisicing elit. Nihil, voluptate.'
-						interpretation='	Lorem ipsum dolor sit amet consectetur
-										adipisicing elit. Temporibus reiciendis
-										aperiam placeat nobis, dolorum laborum,
-										nemo, eos quidem esse ducimus expedita
-										amet repellendus nesciunt enim. Quae
-										pariatur numquam aliquam doloremque.'
-					/>
-				</Stack>
-			</Box>
+			<PageLayout
+				calculateResult={calculateResult}
+				setResult={setResult}
+				title={title}
+				formContent={formContent}
+				result={result}
+				resultDescription={resultDescription}
+				description={description}
+				source={source}
+				interpretation={interpretation}
+			/>
 		</>
 	)
 }
--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,6 +1,61 @@
+import { Box, Flex, Heading, Spacer, Stack } from '@chakra-ui/react'
+import FavButton from '../components/FavButton'
+import FormCard from '../components/FormCard'
+import ResultCard from '../components/ResultCard'
+import DetailsCard from '../components/DetailsCard'
 
-export default function PageLayout() {
-  return (
-    <div>PageLayout</div>
-  )
+interface Props {
+	calculateResult: any
+	setResult: any
+	title: JSX.Element
+	formContent: JSX.Element
+	result: number
+	resultDescription: string
+	description: string
+	source: string
+	interpretation: string
+}
+
+export default function PageLayout({
+	calculateResult,
+	setResult,
+	title,
+	formContent,
+	result,
+	resultDescription,
+	description,
+	source,
+	interpretation,
+}: Props) {
+	return (
+		<Box
+			maxW='650px'
+			mx='auto'
+			px={4}
+			py={8}
+			onChange={() => calculateResult(setResult)}>
+			<Stack spacing={8}>
+				<Flex>
+					<Heading as='h1'>{title} </Heading>
+
+					<Spacer />
+
+					<FavButton />
+				</Flex>
+
+				<FormCard formContent={formContent} />
+
+				<ResultCard
+					result={result}
+					resultDescription={resultDescription}
+				/>
+
+				<DetailsCard
+					description={description}
+					source={source}
+					interpretation={interpretation}
+				/>
+			</Stack>
+		</Box>
+	)
 }
