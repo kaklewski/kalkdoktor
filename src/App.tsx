40d790b83e3c458450d8bb8e6ebe9330e8cc67d3
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-08-23 21:40:03 +0200

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import Navbar from './components/Navbar'
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom'
 import Skala_CHA2DS2_VASc from './pages/Skala_CHA2DS2_VASc'
+import RootLayout from './layouts/RootLayout'
+import Index from './pages/Index'
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<RootLayout />}>
+			<Route index element={<Index />} />
+			<Route path='skala-cha2ds2-vasc' element={<Skala_CHA2DS2_VASc />} />
+		</Route>
+	)
+)
 
 export default function App() {
-	return (
-		<>
-			<Navbar />
-			<Skala_CHA2DS2_VASc />
-		</>
-	)
+	return <RouterProvider router={router} />
 }
---
+++ src/pages/Index.tsx
@@ -0,0 +1,5 @@
+export default function Index() {
+  return (
+    <a href="/skala-cha2ds2-vasc">skala-cha2ds2-vasc</a>
+  )
+}
