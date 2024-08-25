c2d71ce9cd965cb37879f4ebc94e90ccfef2c6cf
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-08-25 16:53:15 +0200

    fix: change icon size

--- src/components/FavButton.tsx
+++ src/components/FavButton.tsx
@@ -1,4 +1,4 @@
-import { Icon, IconButton } from "@chakra-ui/react";
+import { Icon, IconButton } from '@chakra-ui/react'
 
 export default function FavButton() {
 	return (
@@ -6,21 +6,19 @@ export default function FavButton() {
 			aria-label='Dodaj do ulubionych'
 			colorScheme='teal'
 			icon={
-				<Icon>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						width='24'
-						height='24'
-						viewBox='0 0 24 24'
-						fill='none'
-						stroke='currentColor'
-						strokeWidth='2'
-						strokeLinecap='round'
-						strokeLinejoin='round'>
-						<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-						<path d='M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572' />
-					</svg>
-				</Icon>
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					width='24'
+					height='24'
+					viewBox='0 0 24 24'
+					fill='none'
+					stroke='currentColor'
+					strokeWidth='1.5'
+					strokeLinecap='round'
+					strokeLinejoin='round'>
+					<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+					<path d='M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572' />
+				</svg>
 			}
 		/>
 	)
