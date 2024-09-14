2992bc02abe2a627d701a83a5bd436fba604f4da
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-09-15 00:29:55 +0200

    feat: visual adjustments

--- src/components/FavButton.tsx
+++ src/components/FavButton.tsx
@@ -30,21 +30,6 @@ const removeFromFavIcon = (
 		<path stroke='none' d='M0 0h24v24H0z' fill='none' />
 		<path d='M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z' />
 	</svg>
-	// <svg
-	// 	xmlns='http://www.w3.org/2000/svg'
-	// 	width='24'
-	// 	height='24'
-	// 	viewBox='0 0 24 24'
-	// 	fill='none'
-	// 	stroke='currentColor'
-	// 	strokeWidth='1.5'
-	// 	strokeLinecap='round'
-	// 	strokeLinejoin='round'>
-	// 	<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-	// 	<path d='M19.5 12.572l-.5 .428m-6 6l-1 1l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572' />
-	// 	<path d='M22 22l-5 -5' />
-	// 	<path d='M17 22l5 -5' />
-	// </svg>
 )
 
 const isAlreadyFaved = (() => {
@@ -101,8 +86,9 @@ export default function FavButton() {
 
 	return (
 		<IconButton
-			aria-label='Dodaj do ulubionych'
-			colorScheme='teal'
+			aria-label={isFav ? 'Usuń z ulubionych' : 'Dodaj do ulubionych'}
+			variant='outline'
+			colorScheme={isFav ? 'red' : 'teal'}
 			icon={isFav ? removeFromFavIcon : addToFavIcon}
 			onClick={addToFav}
 		/>
