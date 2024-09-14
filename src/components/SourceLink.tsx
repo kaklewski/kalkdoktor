95b466a575582f2bf3a0cc49928546610e8f3fb1
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-09-14 19:45:35 +0200

    refactor: create a separate component for the icon

--- src/components/SourceLink.tsx
+++ src/components/SourceLink.tsx
@@ -5,27 +5,30 @@ interface Props {
 	link: string
 }
 
+const externalLinkIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		width='20'
+		height='20'
+		viewBox='0 0 24 24'
+		fill='none'
+		stroke='currentColor'
+		strokeWidth='1.52'
+		strokeLinecap='round'
+		strokeLinejoin='round'>
+		<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+		<path d='M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6' />
+		<path d='M11 13l9 -9' />
+		<path d='M15 4h5v5' />
+	</svg>
+)
+
 export default function SourceLink({ name, link }: Props) {
 	return (
 		<Link href={link} isExternal>
 			<Stack direction='row'>
 				<span>{name}</span>
-
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					width='20'
-					height='20'
-					viewBox='0 0 24 24'
-					fill='none'
-					stroke='currentColor'
-					strokeWidth='1.52'
-					strokeLinecap='round'
-					strokeLinejoin='round'>
-					<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-					<path d='M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6' />
-					<path d='M11 13l9 -9' />
-					<path d='M15 4h5v5' />
-				</svg>
+				{externalLinkIcon}
 			</Stack>
 		</Link>
 	)
