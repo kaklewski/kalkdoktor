fa7c2867b9bad1875106ce906a96abf2ac2bc2ff
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-09-14 23:43:18 +0200

    feat: adding items to favorites

--- src/components/FavButton.tsx
+++ src/components/FavButton.tsx
@@ -1,25 +1,110 @@
-import { IconButton } from '@chakra-ui/react'
+import { IconButton, useToast } from '@chakra-ui/react'
+import { useState } from 'react'
+
+const pageId = window.location.pathname.slice(1)
+const localStorageKey = 'favorites'
+
+const addToFavIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		width='24'
+		height='24'
+		viewBox='0 0 24 24'
+		fill='none'
+		stroke='currentColor'
+		strokeWidth='1.5'
+		strokeLinecap='round'
+		strokeLinejoin='round'>
+		<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+		<path d='M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572' />
+	</svg>
+)
+
+const removeFromFavIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		width='24'
+		height='24'
+		viewBox='0 0 24 24'
+		fill='currentColor'>
+		<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+		<path d='M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z' />
+	</svg>
+	// <svg
+	// 	xmlns='http://www.w3.org/2000/svg'
+	// 	width='24'
+	// 	height='24'
+	// 	viewBox='0 0 24 24'
+	// 	fill='none'
+	// 	stroke='currentColor'
+	// 	strokeWidth='1.5'
+	// 	strokeLinecap='round'
+	// 	strokeLinejoin='round'>
+	// 	<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+	// 	<path d='M19.5 12.572l-.5 .428m-6 6l-1 1l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572' />
+	// 	<path d='M22 22l-5 -5' />
+	// 	<path d='M17 22l5 -5' />
+	// </svg>
+)
+
+const isAlreadyFaved = (() => {
+	const favString = localStorage.getItem(localStorageKey)
+	if (favString === null) return false
+
+	let favorites = JSON.parse(favString)
+	if (!favorites.includes(pageId)) return false
+
+	return true
+})()
 
 export default function FavButton() {
+	const [isFav, setIsFav] = useState(isAlreadyFaved)
+	const toast = useToast()
+
+	function showToast(type: string) {
+		toast({
+			title:
+				type === 'added'
+					? 'Dodano do ulubionych.'
+					: 'Usunięto z ulubionych.',
+			status: type === 'added' ? 'success' : 'warning',
+			position: 'top',
+			duration: 1500,
+			isClosable: true,
+		})
+	}
+
+	function addToFav() {
+		const favString = localStorage.getItem(localStorageKey)
+
+		if (favString === null) {
+			localStorage.setItem(localStorageKey, JSON.stringify([pageId]))
+			return
+		}
+
+		let favorites = JSON.parse(favString)
+
+		if (favorites.includes(pageId)) {
+			const index = favorites.indexOf(pageId)
+			const removedItem = favorites.splice(index, 1)
+			showToast('removed')
+			setIsFav(false)
+		} else {
+			favorites.push(pageId)
+			favorites.sort()
+			showToast('added')
+			setIsFav(true)
+		}
+
+		localStorage.setItem(localStorageKey, JSON.stringify(favorites))
+	}
+
 	return (
 		<IconButton
 			aria-label='Dodaj do ulubionych'
 			colorScheme='teal'
-			icon={
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					width='24'
-					height='24'
-					viewBox='0 0 24 24'
-					fill='none'
-					stroke='currentColor'
-					strokeWidth='1.5'
-					strokeLinecap='round'
-					strokeLinejoin='round'>
-					<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-					<path d='M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572' />
-				</svg>
-			}
+			icon={isFav ? removeFromFavIcon : addToFavIcon}
+			onClick={addToFav}
 		/>
 	)
 }
