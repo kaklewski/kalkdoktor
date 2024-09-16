002e4fb82a82a948021090380ad18ac0484f81d3
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-09-16 17:42:29 +0200

    fix: tweak logo letter spacing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,7 +41,7 @@ export default function Navbar() {
 				<Flex align='center' justify='space-between'>
 					<Box>
 						<a href='/' id='navLogo'>
-							<Flex align='center' gap={0.5}>
+							<Flex align='center' gap={0}>
 								<svg
 									xmlns='http://www.w3.org/2000/svg'
 									width='24'
