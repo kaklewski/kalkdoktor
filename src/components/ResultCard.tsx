db92097f54dcadcf24c792aa70088ad84db7c71f
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-09-18 15:46:15 +0200

    feat: add scale up and down animation when result changes

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -6,6 +6,7 @@ import {
 	Heading,
 	Text,
 } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 
 interface ComponentProps {
 	result: number
@@ -16,10 +17,24 @@ export default function ResultCard({
 	result,
 	interpretResult,
 }: ComponentProps) {
+	const [isFirstRender, setIsFirstRender] = useState<boolean>(true)
 	const resultInterpretation = interpretResult(result)
 
+	useEffect(() => {
+		if (isFirstRender) {
+			setIsFirstRender(false)
+			return
+		}
+
+		const resultCard = document.getElementById('resultCard')
+		resultCard?.classList.add('animation')
+		setTimeout(() => {
+			resultCard?.classList.remove('animation')
+		}, 300)
+	}, [result])
+
 	return (
-		<Card overflow='hidden' variant='filled'>
+		<Card overflow='hidden' variant='filled' id='resultCard'>
 			<CardHeader>
 				<Heading size='md'>Wynik: {result}</Heading>
 			</CardHeader>
