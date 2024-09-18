33be53c08dc5e37637452518b24e2b3d816c065d
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-09-18 22:56:01 +0200

    feat: add automatic fav button refreshment on both tabs

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,6 +12,7 @@ import {
 } from '@chakra-ui/react'
 import CalculatorCard from '../components/CalculatorCard'
 import CustomAccordionItem from '../components/CustomAccordionItem'
+import { useState } from 'react'
 
 const sortedCalculators = calculators.sort((a: Calculator, b: Calculator) => {
 	const keyA = a.name.toLowerCase()
@@ -22,6 +23,10 @@ const sortedCalculators = calculators.sort((a: Calculator, b: Calculator) => {
 })
 
 export default function Index() {
+	// These two keys exist to make sure that when you add a calculator to favorites on one tab, it is also displayed as faved on the other one
+	const [keyA, setKeyA] = useState<number>(0)
+	const [keyB, setKeyB] = useState<number>(0)
+
 	return (
 		<Box maxW='650px' mx='auto' px={4} py={8}>
 			<Card variant='outline'>
@@ -32,8 +37,10 @@ export default function Index() {
 					</TabList>
 
 					<TabPanels>
-						<TabPanel>
-							<Accordion defaultIndex={[0]} allowMultiple>
+						<TabPanel
+							key={keyA}
+							onClick={() => setKeyB(k => k + 1)}>
+							<Accordion>
 								{categories.map(category => (
 									<CustomAccordionItem
 										key={category}
@@ -61,7 +68,9 @@ export default function Index() {
 								))}
 							</Accordion>
 						</TabPanel>
-						<TabPanel>
+						<TabPanel
+							key={keyB}
+							onClick={() => setKeyA(k => k + 1)}>
 							<Stack spacing={4}>
 								{sortedCalculators.map(calculator => (
 									<CalculatorCard
