504224338cf1bb89f7730d002bcd77b7be488fc0
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-09-19 13:19:46 +0200

    refactor: move list tabs to a separate component

---
+++ src/layouts/ListTabsLayout.tsx
@@ -0,0 +1,70 @@
+import {
+	Heading,
+	Stack,
+	Tab,
+	TabList,
+	TabPanel,
+	TabPanels,
+	Tabs,
+} from '@chakra-ui/react'
+import CalculatorCard from '../components/CalculatorCard'
+import { useState } from 'react'
+import { Calculator, categories } from '../calculators'
+
+interface ComponentProps {
+	calculators: Calculator[]
+}
+
+export default function ListTabsLayout({ calculators }: ComponentProps) {
+	// These two keys exist to make sure that when you add a calculator to favorites on one tab, it is also displayed as faved on the other one
+	const [keyA, setKeyA] = useState<number>(0)
+	const [keyB, setKeyB] = useState<number>(0)
+
+	return (
+		<Tabs colorScheme='teal' isFitted>
+			<TabList mb={3}>
+				<Tab>Alfabetycznie</Tab>
+				<Tab>Kategorie</Tab>
+			</TabList>
+
+			<TabPanels>
+				<TabPanel px={0} key={keyA} onClick={() => setKeyB(k => k + 1)}>
+					<Stack spacing={6}>
+						{calculators.map(calculator => (
+							<CalculatorCard
+								key={calculator.id}
+								id={calculator.id}
+								name={calculator.name}
+								link={calculator.link}
+								description={calculator.description}
+							/>
+						))}
+					</Stack>
+				</TabPanel>
+				<TabPanel px={0} key={keyB} onClick={() => setKeyA(k => k + 1)}>
+					<Stack spacing={6}>
+						{categories.map(category => (
+							<>
+								<Heading as='h2' fontSize='2xl' mt={2}>
+									{category[0].toUpperCase() +
+										category.slice(1)}
+								</Heading>
+								{calculators
+									.filter(calc => calc.category === category)
+									.map(calculator => (
+										<CalculatorCard
+											key={calculator.id}
+											id={calculator.id}
+											name={calculator.name}
+											link={calculator.link}
+											description={calculator.description}
+										/>
+									))}
+							</>
+						))}
+					</Stack>
+				</TabPanel>
+			</TabPanels>
+		</Tabs>
+	)
+}
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,15 +1,5 @@
-import { Calculator, calculators, categories } from '../calculators'
-import {
-	Heading,
-	Stack,
-	Tab,
-	TabList,
-	TabPanel,
-	TabPanels,
-	Tabs,
-} from '@chakra-ui/react'
-import CalculatorCard from '../components/CalculatorCard'
-import { useState } from 'react'
+import { Calculator, calculators } from '../calculators'
+import ListTabsLayout from '../layouts/ListTabsLayout'
 
 const sortedCalculators = calculators.sort((a: Calculator, b: Calculator) => {
 	const keyA = a.name.toLowerCase()
@@ -20,67 +10,5 @@ const sortedCalculators = calculators.sort((a: Calculator, b: Calculator) => {
 })
 
 export default function IndexPage() {
-	// These two keys exist to make sure that when you add a calculator to favorites on one tab, it is also displayed as faved on the other one
-	const [keyA, setKeyA] = useState<number>(0)
-	const [keyB, setKeyB] = useState<number>(0)
-
-	return (
-		<>
-			<Tabs colorScheme='teal' isFitted>
-				<TabList mb={3}>
-					<Tab>Alfabetycznie</Tab>
-					<Tab>Kategorie</Tab>
-				</TabList>
-
-				<TabPanels>
-					<TabPanel
-						px={0}
-						key={keyA}
-						onClick={() => setKeyB(k => k + 1)}>
-						<Stack spacing={6}>
-							{sortedCalculators.map(calculator => (
-								<CalculatorCard
-									key={calculator.id}
-									id={calculator.id}
-									name={calculator.name}
-									link={calculator.link}
-									description={calculator.description}
-								/>
-							))}
-						</Stack>
-					</TabPanel>
-					<TabPanel
-						px={0}
-						key={keyB}
-						onClick={() => setKeyA(k => k + 1)}>
-						<Stack spacing={6}>
-							{categories.map(category => (
-								<>
-									<Heading as='h2' mt={4} mb={1}>
-										{category[0].toUpperCase() +
-											category.slice(1)}
-									</Heading>
-									{sortedCalculators
-										.filter(
-											calc => calc.category === category
-										)
-										.map(calculator => (
-											<CalculatorCard
-												key={calculator.id}
-												id={calculator.id}
-												name={calculator.name}
-												link={calculator.link}
-												description={
-													calculator.description
-												}
-											/>
-										))}
-								</>
-							))}
-						</Stack>
-					</TabPanel>
-				</TabPanels>
-			</Tabs>
-		</>
-	)
+	return <ListTabsLayout calculators={sortedCalculators} />
 }
