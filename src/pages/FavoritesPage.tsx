a0267665acee1bb67e2c9ac38991eb7242bc59ae
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-09-20 21:06:30 +0200

    feat: change tab view to a list view

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,6 +1,35 @@
-import { sortedCalculators } from "../data/calculators-and-categories";
-import ListTabsLayout from "../layouts/ListTabsLayout";
+import { Heading, Stack, Text } from '@chakra-ui/react'
+import { sortedCalculators } from '../data/calculators-and-categories'
+import CalculatorCard from '../components/CalculatorCard'
 
 export default function FavoritesPage() {
-  return <ListTabsLayout calculators={sortedCalculators} />
+	const favIds = (() => {
+		const fav = localStorage.getItem('favorites')
+		if (fav === null) return []
+		return JSON.parse(fav)
+	})()
+	const favoriteCalculators = sortedCalculators.filter(c =>
+		favIds.includes(c.id)
+	)
+
+	return (
+		<>
+			<Heading>Ulubione</Heading>
+			<Stack spacing={4}>
+				{favoriteCalculators.length === 0 ? (
+					<Text>Brak ulubionych</Text>
+				) : (
+					favoriteCalculators.map(calculator => (
+						<CalculatorCard
+							key={calculator.id}
+							id={calculator.id}
+							name={calculator.name}
+							link={calculator.link}
+							description={calculator.description}
+						/>
+					))
+				)}
+			</Stack>
+		</>
+	)
 }
