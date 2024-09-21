f1db5fadca4e7c97d2aa11c68b8af52ce3ef6747
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-09-21 17:02:44 +0200

    feat: add a placeholder when there are no favs

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,6 +1,7 @@
-import { Heading, Stack, Text } from '@chakra-ui/react'
+import { Heading, Stack, Text, VStack } from '@chakra-ui/react'
 import { sortedCalculators } from '../data/calculators-and-categories'
 import CalculatorCard from '../components/CalculatorCard'
+import { IconHeartOff } from '@tabler/icons-react'
 
 export default function FavoritesPage() {
 	const favIds = (() => {
@@ -12,12 +13,25 @@ export default function FavoritesPage() {
 		favIds.includes(c.id)
 	)
 
+	function NoFavoritesPlaceholder() {
+		return (
+			<VStack my={10} mx='auto' maxW='80%'>
+				<IconHeartOff stroke={1.5} size={100} />
+				<Heading size='md'>Brak ulubionych</Heading>
+				<Text align='center'>
+					Możesz dodać kalkulatory do ulubionych klikając ikonę serca.
+				</Text>
+			</VStack>
+		)
+	}
+
 	return (
 		<>
 			<Heading>Ulubione</Heading>
 			<Stack spacing={4}>
 				{favoriteCalculators.length === 0 ? (
-					<Text>Brak ulubionych</Text>
+					// <Text>Brak ulubionych</Text>
+					<NoFavoritesPlaceholder />
 				) : (
 					favoriteCalculators.map(calculator => (
 						<CalculatorCard
