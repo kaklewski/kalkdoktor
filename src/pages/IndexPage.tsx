deae34e17670dc9a8d63b8615751cf77025ac161
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-09-22 19:07:01 +0200

    refactor: move list tabs layout content directly to the index page

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,6 +1,67 @@
+import {
+	Heading,
+	Stack,
+	Tab,
+	TabList,
+	TabPanel,
+	TabPanels,
+	Tabs,
+} from '@chakra-ui/react'
 import { sortedCalculators } from '../data/calculators-and-categories'
-import ListTabsLayout from '../layouts/ListTabsLayout'
+import CalculatorCard from '../components/CalculatorCard'
+import { useState } from 'react'
+import { categories } from '../data/calculators-and-categories'
 
 export default function IndexPage() {
-	return <ListTabsLayout calculators={sortedCalculators} />
+	// These two keys exist to make sure that when you add a calculator to favorites on one tab, it is also displayed as faved on the other one
+	const [keyA, setKeyA] = useState<number>(0)
+	const [keyB, setKeyB] = useState<number>(0)
+
+	return (
+		<Tabs colorScheme='teal' isFitted>
+			<TabList>
+				<Tab>Alfabetycznie</Tab>
+				<Tab>Kategorie</Tab>
+			</TabList>
+
+			<TabPanels>
+				<TabPanel px={0} key={keyA} onClick={() => setKeyB(k => k + 1)}>
+					<Stack spacing={4}>
+						{sortedCalculators.map(calculator => (
+							<CalculatorCard
+								key={calculator.id}
+								id={calculator.id}
+								name={calculator.name}
+								link={calculator.link}
+								description={calculator.description}
+							/>
+						))}
+					</Stack>
+				</TabPanel>
+				<TabPanel px={0} key={keyB} onClick={() => setKeyA(k => k + 1)}>
+					<Stack spacing={8}>
+						{categories.map((category, categoryId) => (
+							<Stack key={categoryId} spacing={4}>
+								<Heading as='h2' fontSize='2xl' ml={2}>
+									{category[0].toUpperCase() +
+										category.slice(1)}
+								</Heading>
+								{sortedCalculators
+									.filter(calc => calc.category === category)
+									.map(calculator => (
+										<CalculatorCard
+											key={calculator.id}
+											id={calculator.id}
+											name={calculator.name}
+											link={calculator.link}
+											description={calculator.description}
+										/>
+									))}
+							</Stack>
+						))}
+					</Stack>
+				</TabPanel>
+			</TabPanels>
+		</Tabs>
+	)
 }
