a7fa19df64085de4e8b43e8f0cd173c269b880f3
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-10-06 20:57:35 +0200

    refactor: replace function calling with useEffect

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,18 +1,19 @@
 import { IconButton, Tooltip, useColorMode } from '@chakra-ui/react'
 import { IconMoon, IconSun } from '@tabler/icons-react'
+import { useEffect } from 'react'
 
 export default function ThemeButton() {
 	const { colorMode, toggleColorMode } = useColorMode()
 
-	// The function below adds a data-theme attribute to the page body. This lets the CSS auto adjust the theme and prevent incorrect theme flashing when switching pages. IDentical function is hardcoded in the HTML directly, which handles setting a proper theme during the initial load.
-	function addThemeAttributeToBody() {
-		const theme = localStorage.getItem('chakra-ui-color-mode')
+	// The useEffect below adds a data-theme attribute to the page body. This lets the CSS auto-adjust the theme and prevent incorrect theme flashing when switching pages. Identical function is hardcoded in the HTML directly, which handles setting a proper theme during the initial load.
+	useEffect(() => {
 		const body = document.querySelector('body')
 		if (body === null) return
 
+		const theme = localStorage.getItem('chakra-ui-color-mode')
 		if (theme === 'light') body.setAttribute('data-theme', 'light')
 		if (theme === 'dark') body.setAttribute('data-theme', 'dark')
-	}
+	}, [colorMode])
 
 	return (
 		<Tooltip
@@ -22,7 +23,6 @@ export default function ThemeButton() {
 			<IconButton
 				onClick={() => {
 					toggleColorMode()
-					addThemeAttributeToBody()
 				}}
 				aria-label={`Zmień motyw na ${
 					colorMode === 'light' ? 'ciemny' : 'jasny'
