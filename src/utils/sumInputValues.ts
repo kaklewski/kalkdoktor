bbd17105a28d82df2fb4bf7652fb16ed11843c6b
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-10-07 22:53:37 +0200

    fix: add explicit type to 'inputs'

--- src/utils/sumInputValues.ts
+++ src/utils/sumInputValues.ts
@@ -1,8 +1,7 @@
-// typ do zmiany
 export function sumInputValues(setResult: (value: number) => void) {
-	const inputs = document.querySelectorAll('input')
+	const inputs: NodeListOf<HTMLInputElement> =
+		document.querySelectorAll('input')
 	let sum: number = 0
-
 	inputs.forEach(input => {
 		if (input.checked) sum += parseInt(input.value)
 	})
