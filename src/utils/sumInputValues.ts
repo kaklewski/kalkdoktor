42d52c5d67ff036439b85dd5306267dc040a54ea
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-10-20 20:57:21 +0200

    fix: change the function to sum up only numbers

--- src/utils/sumInputValues.ts
+++ src/utils/sumInputValues.ts
@@ -2,8 +2,11 @@ export default function sumInputValues(setResult: (value: number) => void) {
 	const inputs: NodeListOf<HTMLInputElement> =
 		document.querySelectorAll('input')
 	let sum: number = 0
+
 	inputs.forEach(input => {
-		if (input.checked) sum += parseInt(input.value)
+		if (input.checked && isNaN(parseInt(input.value)) === false) {
+			sum += parseInt(input.value)
+		}
 	})
 
 	setResult(sum)
