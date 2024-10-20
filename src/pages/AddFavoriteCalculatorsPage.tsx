0e0bf321b729cc36656a69094f58df5bf2bc3f0b
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-10-20 18:54:49 +0200

    refactor: change variable names and wording for cleaner code and better understanding

--- src/pages/AddFavoriteCalculatorsPage.tsx
+++ src/pages/AddFavoriteCalculatorsPage.tsx
@@ -19,78 +19,79 @@ import useDocumentTitle from '../hooks/useDocumentTitle'
 export default function AddFavPage() {
 	useDocumentTitle('Importuj ulubione kalkulatory')
 
-	const urlParams = new URLSearchParams(window.location.search)
-	const favIds = urlParams.get('id')
-
 	useEffect(() => {
-		if (
-			favIds === null ||
-			favIds === undefined ||
-			favIds === '' ||
-			JSON.parse(favIds).length === 0
-		) {
+		if (!idsAreValid) {
 			window.location.href = '/'
 		}
 	}, [])
 
-	const favsToBeAdded =
-		favIds === null
-			? []
-			: sortedCalculators.filter(c => JSON.parse(favIds).includes(c.id))
+	const urlParams = new URLSearchParams(window.location.search)
+	const favoriteCalculatorIds = urlParams.get('id')
+	const idsAreValid: boolean =
+		favoriteCalculatorIds === null ||
+		favoriteCalculatorIds === undefined ||
+		favoriteCalculatorIds === '' ||
+		JSON.parse(favoriteCalculatorIds).length === 0
+			? false
+			: true
 
-	function saveNewFav(fav: string) {
-		localStorage.setItem('favorites', fav)
+	const favoritesToImport =
+		favoriteCalculatorIds === null
+			? []
+			: sortedCalculators.filter(calculator =>
+					JSON.parse(favoriteCalculatorIds).includes(calculator.id)
+			  )
+
+	function importFavorites(favorites: string) {
+		localStorage.setItem('favorites', favorites)
 		window.location.href = '/ulubione'
 	}
 
 	return (
 		<>
-			{favIds === null ||
-				favIds === undefined ||
-				favIds === '' ||
-				(JSON.parse(favIds).length !== 0 && (
-					<Card variant='outline'>
-						<CardBody>
-							<VStack mx='auto' maxW='80%'>
-								<IconHeartPlus stroke={1.5} size={100} />
-								<Heading mx='auto' size='md'>
-									Importuj ulubione
-								</Heading>
-								<Text align='center'>
-									Poniższe kalkulatory zostaną dodane do
-									ulubionych. Twoje obecne ulubione zostaną
-									nadpisane.
-								</Text>
-								<UnorderedList>
-									{favsToBeAdded.map(fav => {
-										return (
-											<ListItem key={fav.id}>
-												{fav.name}
-											</ListItem>
-										)
-									}, [])}
-								</UnorderedList>
-							</VStack>
-						</CardBody>
+			{idsAreValid && (
+				<Card variant='outline'>
+					<CardBody>
+						<VStack mx='auto' maxW='80%'>
+							<IconHeartPlus stroke={1.5} size={100} />
+							<Heading mx='auto' size='md'>
+								Importuj ulubione
+							</Heading>
+							<Text align='center'>
+								Poniższe kalkulatory zostaną dodane do
+								ulubionych na tym urządzeniu. Jeśli masz już
+								jakieś ulubione kalkulatory, zostaną one
+								nadpisane.
+							</Text>
+							<UnorderedList>
+								{favoritesToImport.map(fav => {
+									return (
+										<ListItem key={fav.id}>
+											{fav.name}
+										</ListItem>
+									)
+								}, [])}
+							</UnorderedList>
+						</VStack>
+					</CardBody>
 
-						<CardFooter>
-							<Flex
-								justify='center'
-								align='center'
-								gap={3}
-								w='100%'>
-								<Link href='/'>
-									<Button>Anuluj</Button>
-								</Link>
-								<Button
-									colorScheme='teal'
-									onClick={() => saveNewFav(favIds)}>
-									Dodaj
-								</Button>
-							</Flex>
-						</CardFooter>
-					</Card>
-				))}
+					<CardFooter mb={2}>
+						<Flex justify='center' align='center' gap={3} w='100%'>
+							<Link href='/'>
+								<Button>Anuluj</Button>
+							</Link>
+							<Button
+								colorScheme='red'
+								onClick={() => {
+									if (favoriteCalculatorIds !== null)
+										importFavorites(favoriteCalculatorIds)
+								}}>
+								Importuj
+							</Button>
+						</Flex>
+					</CardFooter>
+				</Card>
+			)}
 		</>
 	)
 }
