dd553b87973d595845272cbd260e6009d8898684
Oskar <oskarkaklewski@gmail.com>
2025-01-02 23:10:36 +0100

    feat: Replace PWA manifest with VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Kalkdoktor',
+        short_name: 'Kalkdoktor',
+        lang: 'pl',
+        description:
+          'Darmowa, otwartoźródłowa aplikacja webowa zawierająca kalkulatory, skale i konwertery przeznaczone dla lekarzy. Ułatwia diagnozowanie pacjenta poprzez szybki dostęp do niezbędnych narzędzi.',
+        start_url: 'index.html',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#81e6d9',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
