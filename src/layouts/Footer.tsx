4c92607667170b504b91a2c4c79c55b95bb2db77
Oskar <oskarkaklewski@gmail.com>
2025-02-23 13:22:58 +0100

    feat: Add footer links

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,20 +1,39 @@
-import { Box, Divider, Flex, Link, Text, Tooltip } from '@chakra-ui/react'
-import { IconBrandGithubFilled } from '@tabler/icons-react'
+import { Box, Divider, Flex, IconButton, Link, Text, Tooltip } from '@chakra-ui/react'
+import { IconBrandGithub, IconBrandLinkedin, IconWorld } from '@tabler/icons-react'
 
 export default function Footer() {
-  const GITHUB_LINK = 'https://github.com/kaklewski/kalkdoktor'
   const currentYear = new Date().getFullYear()
 
   return (
     <Box as='footer' maxW='768px' mx='auto' paddingInline={{ base: 0, md: 4 }}>
       <Divider />
-      <Flex p={4} fontSize='xs' direction='column' justify='center' align='center' gap={1}>
-        <Text>&copy; {currentYear} Oskar Kąklewski - Opublikowano na licencji GPL 3.0</Text>
-        <Tooltip label='Zobacz repozytorium na GitHubie'>
-          <Link href={GITHUB_LINK}>
-            <IconBrandGithubFilled size={16} stroke={1.5} />
-          </Link>
-        </Tooltip>
+      <Flex p={4} direction='column' justify='center' align='center'>
+        <Text fontSize='xs'>
+          &copy; {currentYear} Oskar Kąklewski - Opublikowano na licencji GPL 3.0
+        </Text>
+        <Flex>
+          <Tooltip label='GitHub'>
+            <Link href='https://github.com/kaklewski/kalkdoktor' isExternal>
+              <IconButton aria-label='GitHub' variant='ghost' size='sm'>
+                <IconBrandGithub size={20} stroke={1.8} />
+              </IconButton>
+            </Link>
+          </Tooltip>
+          <Tooltip label='Portfolio'>
+            <Link href='https://kaklewski.pl' isExternal>
+              <IconButton aria-label='Portfolio' variant='ghost' size='sm'>
+                <IconWorld size={20} stroke={1.8} />
+              </IconButton>
+            </Link>
+          </Tooltip>
+          <Tooltip label='LinkedIn'>
+            <Link href='https://www.linkedin.com/in/oskar-kaklewski' isExternal>
+              <IconButton aria-label='LinkedIn' variant='ghost' size='sm'>
+                <IconBrandLinkedin size={20} stroke={1.8} />
+              </IconButton>
+            </Link>
+          </Tooltip>
+        </Flex>
       </Flex>
     </Box>
   )
