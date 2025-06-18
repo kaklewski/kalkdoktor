4306f480b53c6b54880e45c18cfd6d4ba2ed1612
Oskar <oskarkaklewski@gmail.com>
2025-06-18 20:04:41 +0200

    refactor: Rename main.scss to styles.scss

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
-import './assets/main.scss'
+import './assets/styles.scss'
 import { theme } from './theme'
 import { MathJaxContext } from 'better-react-mathjax'
 
