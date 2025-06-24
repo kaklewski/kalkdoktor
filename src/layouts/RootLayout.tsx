4436d6fc903fc6a73d53e186e9eab3a2ce34dbc5
Oskar <oskarkaklewski@gmail.com>
2025-06-24 08:14:28 +0200

    feat: Implement scroll-to-top functionality on route change in RootLayout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import { Container, Stack } from '@chakra-ui/react'
+import { useEffect } from 'react'
 
 export default function RootLayout() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <>
       <Navbar />
