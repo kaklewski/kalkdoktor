eea20e0870f4668eb51054b5508be34b903131dc
Oskar <oskarkaklewski@gmail.com>
2025-07-25 16:21:51 +0200

    refactor: Simplify ESLint configuration by removing unused imports and integrating Prettier settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,60 +1,32 @@
-// import js from '@eslint/js';
-// import globals from 'globals';
-// import reactHooks from 'eslint-plugin-react-hooks';
-// import reactRefresh from 'eslint-plugin-react-refresh';
-// import tseslint from 'typescript-eslint';
-// import prettier from 'eslint-plugin-prettier';
-
-// export default [
-//   tseslint.config({
-//     files: ['**/*.{ts,tsx}'],
-//     ignores: ['dist'],
-//     languageOptions: {
-//       ecmaVersion: 2020,
-//       globals: globals.browser,
-//     },
-//     plugins: {
-//       'react-hooks': reactHooks,
-//       'react-refresh': reactRefresh,
-//       prettier: prettier,
-//     },
-//     rules: {
-//       ...reactHooks.configs.recommended.rules,
-//       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-//       'prettier/prettier': 'warn',
-//     },
-//   }),
-//   js.configs.recommended,
-//   {
-//     // Turn off ESLint rules that conflict with Prettier
-//     extends: ['eslint-config-prettier'],
-//   },
-// ];
-
-import js from '@eslint/js';
 import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
-  js.configs.recommended,
+  {
+    files: ['vite.config.ts'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
 
-  tseslint.config({
+  ...tseslint.config({
     files: ['**/*.{ts,tsx}'],
     ignores: ['dist', 'build'],
     languageOptions: {
+      parser: tseslint.parser,
       ecmaVersion: 2020,
       sourceType: 'module',
       globals: globals.browser,
     },
     plugins: {
+      '@typescript-eslint': tseslint.plugin,
       react: react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier: prettier,
     },
     rules: {
       ...react.configs.recommended.rules,
@@ -63,8 +35,6 @@ export default [
       'react/react-in-jsx-scope': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/ban-ts-comment': 'warn',
-      // Prettier as an ESLint rule
-      'prettier/prettier': 'warn',
     },
     settings: {
       react: {
@@ -73,8 +43,5 @@ export default [
     },
   }),
 
-  {
-    // Turn off ESLint rules that conflict with Prettier
-    extends: ['eslint-config-prettier'],
-  },
+  prettierConfig,
 ];
