225e9fa6bcb6ed3acdab46c249ce2bb812d070e0
Oskar <oskarkaklewski@gmail.com>
2025-07-25 10:28:22 +0200

    feat: Add Prettier and ESLint configuration for improved code formatting and linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,80 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// import js from '@eslint/js';
+// import globals from 'globals';
+// import reactHooks from 'eslint-plugin-react-hooks';
+// import reactRefresh from 'eslint-plugin-react-refresh';
+// import tseslint from 'typescript-eslint';
+// import prettier from 'eslint-plugin-prettier';
 
-export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  ignores: ['dist'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+// export default [
+//   tseslint.config({
+//     files: ['**/*.{ts,tsx}'],
+//     ignores: ['dist'],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       globals: globals.browser,
+//     },
+//     plugins: {
+//       'react-hooks': reactHooks,
+//       'react-refresh': reactRefresh,
+//       prettier: prettier,
+//     },
+//     rules: {
+//       ...reactHooks.configs.recommended.rules,
+//       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+//       'prettier/prettier': 'warn',
+//     },
+//   }),
+//   js.configs.recommended,
+//   {
+//     // Turn off ESLint rules that conflict with Prettier
+//     extends: ['eslint-config-prettier'],
+//   },
+// ];
+
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+
+  tseslint.config({
+    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist', 'build'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    plugins: {
+      react: react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettier,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      // Prettier as an ESLint rule
+      'prettier/prettier': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  }),
+
+  {
+    // Turn off ESLint rules that conflict with Prettier
+    extends: ['eslint-config-prettier'],
   },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-})
+];
