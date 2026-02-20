c11f9071cac172977afc0a243f224ec2e6e45998
Oskar Kąklewski <oskarkaklewski@gmail.com>
2026-02-20 23:38:44 +0100

    refactor: Refresh logo on big screens

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -5,6 +5,7 @@ import { Link as RouterLink } from 'react-router-dom';
 
 import ColorModeButton from '../components/buttons/ColorModeButton';
 import SearchCommandModal from '../components/modals/SearchCommandModal';
+import AppTooltip from '../components/other/AppTooltip';
 import Logo from '../components/other/Logo';
 import ROUTES from '../data/routes';
 import STRINGS from '../data/strings';
@@ -12,25 +13,46 @@ import STRINGS from '../data/strings';
 const Navbar = () => (
   <NavbarContainer>
     <Flex alignItems="center" justify="space-between" gap={4} wrap="wrap">
-      <Box
-        as={RouterLink}
-        to={ROUTES.HOME}
-        aria-label={STRINGS.NAV.LINKS.HOMEPAGE}
-        h={{ base: 8, lg: 10 }}
-      >
-        <Logo />
-      </Box>
+      <Hide above="md">
+        <AppTooltip label={STRINGS.NAV.LINKS.HOMEPAGE}>
+          <Box
+            as={RouterLink}
+            to={ROUTES.HOME}
+            aria-label={STRINGS.NAV.LINKS.HOMEPAGE}
+            h={{ base: 8, lg: 10 }}
+          >
+            <Logo />
+          </Box>
+        </AppTooltip>
+      </Hide>
+
+      <Show above="md">
+        <AppTooltip label={STRINGS.NAV.LINKS.HOMEPAGE}>
+          <Button
+            as={RouterLink}
+            to={ROUTES.HOME}
+            aria-label={STRINGS.NAV.LINKS.HOMEPAGE}
+            variant="ghost"
+            p={1}
+            size="lg"
+          >
+            <Logo />
+          </Button>
+        </AppTooltip>
+      </Show>
 
       <Flex alignItems="center" gap={{ base: 3, md: 2 }} wrap="wrap">
         <SearchCommandModal />
 
         <Hide above="md">
-          <IconButton
-            as={RouterLink}
-            to={ROUTES.FAVORITES}
-            aria-label={STRINGS.BUTTONS.FAVORITES.TITLE}
-            icon={<IconHeart stroke={1.5} />}
-          />
+          <AppTooltip label={STRINGS.BUTTONS.FAVORITES.TITLE}>
+            <IconButton
+              as={RouterLink}
+              to={ROUTES.FAVORITES}
+              aria-label={STRINGS.BUTTONS.FAVORITES.TITLE}
+              icon={<IconHeart stroke={1.5} />}
+            />
+          </AppTooltip>
         </Hide>
 
         <Show above="md">
