b1582a6b189b73732315608fb1bc1d0646810b15
Oskar Kąklewski <oskarkaklewski@gmail.com>
2026-02-26 18:29:15 +0100

    chore: Add setup.ts for vitest

---
+++ src/test/setup.ts
@@ -0,0 +1,17 @@
+import { vi } from 'vitest';
+
+window.HTMLElement.prototype.scrollTo = vi.fn();
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
